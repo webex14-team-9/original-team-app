ca80026af5a94c267afbb47d5ebf0d29f6346117
taisei <umekawa0707@gmail.com>
2022-06-05 16:39:42 +0900

    画像の投稿完成

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,11 @@
 <template>
-  <div>
-    <div>
-      <Uploadtext />
-    </div>
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-  </div>
+  <nav>
+    <router-link to="/">Home</router-link> |
+    <router-link to="/about">About</router-link>
+  </nav>
+  <router-view />
 </template>
 
-<script>
-import Uploadtext from "./components/Uploadtext.vue"
-export default {
-  components: {
-    Uploadtext,
-  },
-}
-</script>
 <style>
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
