79812ab58a3ed29e4add94bc6b330241bcd33503
makito98 <shukatsu23th@gmai.com>
2022-06-07 16:36:57 +0900

    ログイン機能途中

--- src/App.vue
+++ src/App.vue
@@ -1,30 +1,41 @@
 <template>
-  <nav>
-    <router-link to="/">Home</router-link> |
-    <router-link to="/about">About</router-link>
-  </nav>
-  <router-view />
+  <div>
+    <div>
+      <router-link to="/">Home</router-link> |
+      <router-link to="/about">About</router-link> |
+      <router-link to="/my-page">MyPage</router-link>
+      <router-link to="/BeforSignIn">サインアウトする</router-link>
+      <router-link to="/AfterSignIn">サインインする</router-link>
+    </div>
+    <router-view />
+    <div v-if="isLoggin">ログインしてます。</div>
+    <div v-else>ログインしてません。</div>
+    <div @click="login">ログイン</div>
+  </div>
 </template>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
+<script>
+import firebase from "firebase"
 
-nav {
-  padding: 30px;
+export default {
+  data() {
+    return {
+      isLoggin: false,
+    }
+  },
+  methods: {
+    login() {
+      const provider = new firebase.auth.GoogleAuthProvider()
+      firebase
+        .auth()
+        .signInWithPopup(provider)
+        .then((result) => {
+          console.log({ result })
+          if (result.user) {
+            this.isLogin = true
+          }
+        })
+    },
+  },
 }
-
-nav a {
-  font-weight: bold;
-  color: #2c3e50;
-}
-
-nav a.router-link-exact-active {
-  color: #42b983;
-}
-</style>
+</script>
