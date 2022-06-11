93f9a2521a1fc2246c090e44134b469c7bd6a046
makito98 <shukatsu23th@gmai.com>
2022-06-11 21:27:58 +0900

    ログイン機能完成しました。

--- src/App.vue
+++ src/App.vue
@@ -4,36 +4,60 @@
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link> |
       <router-link to="/my-page">MyPage</router-link>
-      <router-link to="/BeforSignIn">サインアウトする</router-link>
-      <router-link to="/AfterSignIn">サインインする</router-link>
     </div>
+    <button v-on:click="logout">ログアウト</button>
+    <button @click="login">ログイン</button>
     <router-view />
-    <div v-if="isLoggin">ログインしてます。</div>
-    <div v-else>ログインしてません。</div>
-    <div @click="login">ログイン</div>
   </div>
 </template>
 
 <script>
-import firebase from "firebase"
+import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
+import { signOut } from "firebase/auth"
+const provider = new GoogleAuthProvider()
 
 export default {
   data() {
     return {
-      isLoggin: false,
+      data: null,
     }
   },
   methods: {
     login() {
-      const provider = new firebase.auth.GoogleAuthProvider()
-      firebase
-        .auth()
-        .signInWithPopup(provider)
+      const auth = getAuth()
+      signInWithPopup(auth, provider)
         .then((result) => {
-          console.log({ result })
-          if (result.user) {
-            this.isLogin = true
+          const credential = GoogleAuthProvider.credentialFromResult(result)
+          const token = credential.accessToken
+          const user = result.user
+
+          console.log(token)
+          console.log(user)
+        })
+        .catch((error) => {
+          const errorCode = error.code
+          const errorMessage = error.message
+          const email = error.customData
+          const credential = GoogleAuthProvider.credentialFormError
+
+          console.log(errorCode)
+          console.log(errorMessage)
+          console.log(email)
+          console.log(credential)
+        })
+    },
+    logout() {
+      const auth = getAuth()
+      signOut(auth)
+        .then(() => {
+          // Sign-out successful.
+          return {
+            token: "",
           }
+        })
+        .catch((error) => {
+          // An error happened.
+          console.log(`ログアウトに失敗しました、(${error})`)
         })
     },
   },
