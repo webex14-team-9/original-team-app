bfb53361a89e25117d39da2b676afdcd135dfb5b
makito98 <shukatsu23th@gmai.com>
2022-06-12 22:25:34 +0900

    ログイン機能完成

--- src/App.vue
+++ src/App.vue
@@ -2,64 +2,14 @@
   <div>
     <div>
       <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link> |
-      <router-link to="/my-page">MyPage</router-link>
+      <router-link to="/output">About</router-link> |
+      <router-link to="/my-page">MyPage</router-link>|
+      <router-link to="/logIn">logIn</router-link>
     </div>
-    <button v-on:click="logout">ログアウト</button>
-    <button @click="login">ログイン</button>
-    <router-view />
   </div>
+  <router-view />
 </template>
 
-<script>
-import { getAuth, signInWithPopup, GoogleAuthProvider } from "firebase/auth"
-import { signOut } from "firebase/auth"
-const provider = new GoogleAuthProvider()
+<script></script>
 
-export default {
-  data() {
-    return {
-      data: null,
-    }
-  },
-  methods: {
-    login() {
-      const auth = getAuth()
-      signInWithPopup(auth, provider)
-        .then((result) => {
-          const credential = GoogleAuthProvider.credentialFromResult(result)
-          const token = credential.accessToken
-          const user = result.user
-
-          console.log(token)
-          console.log(user)
-        })
-        .catch((error) => {
-          const errorCode = error.code
-          const errorMessage = error.message
-          const email = error.customData
-          const credential = GoogleAuthProvider.credentialFormError
-
-          console.log(errorCode)
-          console.log(errorMessage)
-          console.log(email)
-          console.log(credential)
-        })
-    },
-    logout() {
-      const auth = getAuth()
-      signOut(auth)
-        .then(() => {
-          // Sign-out successful.
-          return {
-            token: "",
-          }
-        })
-        .catch((error) => {
-          // An error happened.
-          console.log(`ログアウトに失敗しました、(${error})`)
-        })
-    },
-  },
-}
-</script>
+<style></style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomeView from "../views/HomeView.vue"
 import AboutView from "../views/AboutView.vue"
+import LogInView from "../views/LogInView.vue"
 
 const routes = [
   {
@@ -20,6 +21,11 @@ const routes = [
     path: "/my-page",
     name: "MyPage",
     component: AboutView,
+  },
+  {
+    path: "/login",
+    name: "LogIn",
+    component: LogInView,
   },
 ]
 
