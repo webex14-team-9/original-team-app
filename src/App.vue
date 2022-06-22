bae6ad2548300902420ede1af0754e58fa3066c7
taisei <umekawa0707@gmail.com>
2022-06-22 15:18:42 +0900

    メインページ

--- src/App.vue
+++ src/App.vue
@@ -1,14 +1,6 @@
 <template>
   <div id="app">
     <div class="background">
-      <div class="trip">
-        <h2>旅ログ🐾</h2>
-      </div>
-      <div>
-        <div class="click">
-          <button onclick="location.href='Upload.vue'">ログインページ</button>
-        </div>
-      </div>
       <nav id="nav">
         <ul>
           <router-link to="/">Home</router-link>
@@ -17,7 +9,7 @@
           |
         </ul>
       </nav>
-      <div class="text">For the best trip</div>
+
       <router-view />
     </div>
   </div>
@@ -31,33 +23,7 @@
   text-align: center;
   color: #2c3e50;
 }
-button {
-  color: #fff;
-  border: 2px solid #fff;
-  border-radius: 0;
-  background-image: -webkit-gradient(
-    linear,
-    left top,
-    right top,
-    from(#dbb7ff),
-    to(#c6c6ff)
-  );
-  background-image: -webkit-linear-gradient(left, #eee8aa 0%, #c6c6ff 100%);
-  background-image: linear-gradient(to right, #eee8aa 0%, #c6c6ff 100%);
-  -webkit-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
-}
-button:hover {
-  -webkit-transform: translate(0, -2px);
-  transform: translate(0, -2px);
-  color: #fff;
-  -webkit-box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
-  box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
-}
-.trip {
-  margin-top: 30px;
-  font-size: 30px;
-}
+
 .background {
   margin: 0;
   width: 100vw;
@@ -79,13 +45,5 @@ nav a {
 
 nav a.router-link-exact-active {
   color: #42b983;
-}
-.text {
-  margin-top: 5px;
-  font-family: "IM Fell DW Pica SC", serif;
-  font-size: 40px;
-  padding: 30px;
-  font-weight: bold;
-  color: #2c3e50;
 }
 </style>
---
+++ src/views/HomeView.vue
@@ -0,0 +1,54 @@
+<template>
+  <div>
+    <div class="trip">
+      <h2>旅ログ🐾</h2>
+    </div>
+    <div>
+      <div class="click">
+        <button onclick="location.href='Upload.vue'">ログインページ</button>
+      </div>
+    </div>
+    <div class="text">For the best trip</div>
+  </div>
+</template>
+
+<style>
+button {
+  color: #fff;
+  border: 2px solid #fff;
+  border-radius: 0;
+  background-image: -webkit-gradient(
+    linear,
+    left top,
+    right top,
+    from(#dbb7ff),
+    to(#c6c6ff)
+  );
+  background-image: -webkit-linear-gradient(left, #eee8aa 0%, #c6c6ff 100%);
+  background-image: linear-gradient(to right, #eee8aa 0%, #c6c6ff 100%);
+  -webkit-box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+  box-shadow: 0 5px 10px rgba(0, 0, 0, 0.1);
+}
+button:hover {
+  -webkit-transform: translate(0, -2px);
+  transform: translate(0, -2px);
+  color: #fff;
+  -webkit-box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
+  box-shadow: 0 8px 15px rgba(0, 0, 0, 0.2);
+}
+.trip {
+  font-family: "IM Fell DW Pica SC", serif;
+
+  margin-top: 30px;
+  font-size: 30px;
+}
+
+.text {
+  margin-top: 5px;
+  font-family: "IM Fell DW Pica SC", serif;
+  font-size: 40px;
+  padding: 30px;
+  font-weight: bold;
+  color: #2c3e50;
+}
+</style>
