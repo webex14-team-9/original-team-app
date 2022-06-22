bbb97f6224ffd3d9f9d93e1d30683f4f1d069d42
makito98 <shukatsu23th@gmai.com>
2022-06-22 14:32:53 +0900

    デザイン等変更しました。

---
+++ src/views/UserProffile.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="logout-content">
+    <button v-on:click="logout">ログアウト</button>
+  </div>
+</template>
+
+<script>
+import { getAuth, signOut } from "firebase/auth"
+
+export default {
+  methods: {
+    logout() {
+      const auth = getAuth()
+      this.$router.push("/LogIn")
+      signOut(auth)
+        .then(() => {
+          console.log("ログアウトできてます。")
+          // Sign-out successful.
+          return {
+            token: "",
+          }
+        })
+        .catch((error) => {
+          // An error happened.
+          console.log(`ログアウトに失敗しました、(${error})`)
+        })
+    },
+  },
+}
+</script>
